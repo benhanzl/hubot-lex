63d6831fe2d95e05d29f65a37778918d50bdfc8d
Ben Hanzl <ben.hanzl@izea.com>
2017-11-09 15:10:29 -0500

    Create package

---
+++ README.md
@@ -0,0 +1,30 @@
+# hubot-lex
+
+A hubot script that interacts with AWS Lex.
+
+See [`src/hubot-lex.js`](src/hubot-lex.js) for full documentation.
+
+## Installation
+
+In hubot project repo, run:
+
+`npm install hubot-lex --save`
+
+Then add **hubot-lex** to your `external-scripts.json`:
+
+```json
+[
+  "hubot-lex"
+]
+```
+
+## Sample Interaction
+
+```
+hubot> hubot hello
+hubot> hello!
+```
+
+## NPM Module
+
+https://www.npmjs.com/package/hubot-lex
---
+++ src/hubot-lex.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// Description
+//   A hubot script that interacts with AWS Lex
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   hubot hello - <what the respond trigger does>
+//   orly - <what the hear trigger does>
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   Ben Hanzl <ben.hanzl@gmail.com>
+
+module.exports = (robot) => {
+  robot.respond(/hello/, (message) => {
+    message.reply('hello!')
+  })
+
+  robot.hear(/orly/, (message) => {
+    message.send('yarly')
+  })
+}
